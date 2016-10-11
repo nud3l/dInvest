d92dbb7ad16802534c30c9a97e2afbd2b69ef85c
nud3l <dominik.harz@gmail.com>
2016-10-11 13:43:17 +0200

    Update trading

---
+++ trading/eventListener.py
@@ -0,0 +1,5 @@
+# TODO: Needs to listen to events of the organization
+# Event: NewInvestmentByUser (address, value)
+# TODO: Query list (blacklist) of companies from contract (one blacklist for all)
+# TODO: Export financial indicators (return, Sharpe, alpha, beta etc.)
+# TODO: Find financial offer and call function in contract
