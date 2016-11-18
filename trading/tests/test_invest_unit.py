ae0f3ed45802bc991c38682ba12947b9f04e96f0
nud3l <dominik.harz@gmail.com>
2016-11-18 15:55:06 +0100

    Added and updated tests

--- trading/tests/test_invest_unit.py
+++
@@ -1,7 +0,0 @@
-from recommender.InvestHandler import getSectors
-from web3 import Web3
-
-class TestInvestHandler:
-    def test_get_sectors(self):
-        sectors = getSectors()
-        assert sectors
